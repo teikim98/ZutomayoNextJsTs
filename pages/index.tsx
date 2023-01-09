64b9508deefb3dc6668a8cc9a8fa9bae26333a77
teikim98 <teikim98@gmail.com>
2023-01-09 18:48:14 +0900

    bg 수정해야함

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,31 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <h2>this is Home</h2>
+      <div id="ztmy" className="home">
+        <div className="inner-wrapper">
+          <picture className="top-bg">
+            <source media="(min-width: 769px)" srcSet="/main_kv_pc.jpeg" />
+            <source media="(max-width: 769px)" srcSet="/main_kv_sp.jpeg" />
+            <img src="/main_kv_pc.jpeg" alt="bg" />
+          </picture>
+        </div>
+      </div>
+
+
+      <style jsx>{`
+        .home {
+          width: 100vw;
+          max-width: 100%;
+          margin : 0 auto;
+        }
+        img{
+          object-fit:cover
+          overflow:hidden;
+          
+        }
+      `}</style>
     </>
-  )
+  );
 }
