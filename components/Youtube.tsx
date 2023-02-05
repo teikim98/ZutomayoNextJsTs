db26835a9b5855babfc2cd936b48873f33a3c37e
teikim98 <teikim98@gmail.com>
2023-02-05 22:33:46 +0900

    youtube compo

---
+++ components/Youtube.tsx
@@ -0,0 +1,20 @@
+
+
+function Youtube() {
+
+
+    return (
+        <>
+            <div>
+
+            </div>
+            <style jsx>{`
+            
+
+
+        `}</style>
+        </>
+    );
+}
+
+export default Youtube;
