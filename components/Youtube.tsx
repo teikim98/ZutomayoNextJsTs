85f5a32ff81f422684be12b6428e465fedd365b5
teikim98 <teikim98@gmail.com>
2023-02-19 22:42:05 +0900

    styling yt wrapprer

--- components/Youtube.tsx
+++ components/Youtube.tsx
@@ -7,11 +7,17 @@ function Youtube() {
         <>
             <div className="yt-container">
                 <div className="yt-wrapper">
-                    <div className="yt-moveimg"></div>
+                    <div className="yt-moveimg">
+
+                    </div>
                 </div>
             </div>
             <style jsx>{`
-            
+                .yt-container{
+                    z-index : 1000;
+                    right : -100%;
+                    
+                }
 
 
         `}</style>
